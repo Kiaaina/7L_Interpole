f1428c3765e1c7d7962984bf1c5472a6b86e5ba3
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2025-06-24 14:20:24 -0700

    Add files via upload

--- Interpole.cmd
+++ Interpole.cmd
@@ -4,5 +4,6 @@ title HTML
 mode 70,5
 color 04
 cls
+"%UserProfile%\Desktop\7L_Interpole-main\Interpole.html"
 "C:\Program Files\Google\Chrome\Application\chrome.exe" %UserProfile%\Desktop\7L_Interpole-main\Interpole.html
 exit
